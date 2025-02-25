f87198261b6bcdeffcaecfe33b23c80dbcb2d1f8
Gusmasti <gusmaomatheusti@gmail.com>
2025-02-25 20:07:49 -0300

    Add project files.

---
+++ Fiap.Operacoes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Operacoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            String nome = Console.ReadLine();
+
+            Console.Write("\nDigite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("\nOlá " + nome + ", você tem " + idade + " anos.\n");
+
+            Console.Write("\nAgora digite um número: ");
+            double primeiroNumero = double.Parse(Console.ReadLine());
+
+            Console.Write("\nAgora digite outro número: ");
+            double segundoNumero = double.Parse(Console.ReadLine());
+
+            if (primeiroNumero > segundoNumero)
+            {
+                Console.WriteLine($"\n{primeiroNumero} é maior que {segundoNumero}.\n");
+            }
+            else
+            {
+                Console.WriteLine($"\n{segundoNumero} é maior que {primeiroNumero}.\n");
+            }
+
+            Console.WriteLine($"\nA soma dos números é: {primeiroNumero + segundoNumero}.\n");
+            Console.WriteLine($"\nA subtração dos números é: {primeiroNumero - segundoNumero}.\n");
+            Console.WriteLine($"\nA multiplicação dos números é: {primeiroNumero * segundoNumero}.\n");
+            Console.WriteLine($"\nA divisão dos números é: {primeiroNumero / segundoNumero}.\n");
+
+            Console.WriteLine("\nPressione qualquer tecla!");
+            Console.ReadKey();
+        }
+    }
+}
